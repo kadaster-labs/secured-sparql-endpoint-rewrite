eefed8c2991fa28bcdfc3a989cc8d4fe90e3e3df
marcvanandel <marc.vanandel@kadaster.nl>
2023-12-18 17:32:59 +0100

    fuseki module set up

---
+++ src/main/java/nl/kadaster/labs/lock_unlock/fuseki/AuthorizationFilter.java
@@ -0,0 +1,20 @@
+package nl.kadaster.labs.lock_unlock.fuseki;
+
+import org.apache.jena.fuseki.servlets.ActionService;
+import org.apache.jena.fuseki.servlets.HttpAction;
+
+public class AuthorizationFilter extends ActionService {
+
+    @Override
+    public void validate(HttpAction arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validate'");
+    }
+
+    @Override
+    public void execute(HttpAction arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }    
+
+}
