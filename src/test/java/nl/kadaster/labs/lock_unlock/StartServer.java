cdd1622763fc8a2d9e4b071152f41572427c172b
marcvanandel <marc.vanandel@kadaster.nl>
2023-12-19 07:53:23 +0100

    test start server class

---
+++ src/test/java/nl/kadaster/labs/lock_unlock/StartServer.java
@@ -0,0 +1,9 @@
+package nl.kadaster.labs.lock_unlock;
+
+import org.apache.jena.fuseki.main.cmds.FusekiMainCmd;
+
+public class StartServer {
+    public static void main(String[] args) {
+        FusekiMainCmd.main(args);
+    }
+}
