a2fbc7f70a656f1119571b81521ef4edf7254985
marcvanandel <marc.vanandel@kadaster.nl>
2023-12-19 08:45:37 +0100

    run test server

--- src/test/java/nl/kadaster/labs/lock_unlock/StartServer.java
+++ src/test/java/nl/kadaster/labs/lock_unlock/StartServer.java
@@ -1,9 +1,10 @@
 package nl.kadaster.labs.lock_unlock;
 
-import org.apache.jena.fuseki.main.cmds.FusekiMainCmd;
+import org.apache.jena.fuseki.cmd.FusekiCmd;
 
 public class StartServer {
-    public static void main(String[] args) {
-        FusekiMainCmd.main(args);
+    public static void main(String[] _args) {
+        String[] args = { "--mem", "/ds" };
+        FusekiCmd.main(args);
     }
 }
